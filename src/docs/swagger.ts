f1b2ea49cf09452cccba168d78162a70fb259cce
Ashok Babu <pashokbabu.38@gmail.com>
2026-03-10 12:50:17 +0530

    chore: make swagger url relative for production

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -10,8 +10,8 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000',
-        description: 'Development server',
+        url: '/',
+        description: 'Current server',
       },
     ],
   },
